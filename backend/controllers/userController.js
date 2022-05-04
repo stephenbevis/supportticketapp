4d0472adb58c195c8df0bad8f541661f7e27541d
Stephen Bevis <stevbev95@gmail.com>
2022-05-04 13:39:49 -0600

    Added Login Functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,22 @@
 // Imports
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 // Models
 const User = require('../models/userModel')
 
+// Generat Json Web Token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
+
 // @desc   Register User
 // @route  /api/users
 // @access Public
+
 const registerUser = asyncHandler(async (req, res) => {
     const { firstname, lastname, email, password } = req.body
 
@@ -21,9 +30,11 @@ const registerUser = asyncHandler(async (req, res) => {
     // Does User Already Exist?
     const userExists = await User.findOne({ email })
 
-    userExists && res.status(400).json({
-        message: 'User already exists'
-    })
+    if(userExists){ 
+        return res.status(400).json({
+            message: 'User already exists'
+        })
+    }
 
     // Hash Password
     const salt = await bcrypt.genSalt(10)
@@ -42,7 +53,8 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             firstname: user.firstname,
             lastname: user.lastname,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400).json({
@@ -54,13 +66,32 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc   Login User
 // @route  /api/users/login
 // @access Public
+
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    // Confirm User Exists and Password Matches
+    if(user && (await bcrypt.compare(password, user.password))){
+        return res.status(200).json({
+            _id: user._id,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401).json({
+            message: 'Invalid credentials'
+        })
+    }
 })
 
 // @desc   Get Users
 // @route  /api/users
 // @access Public
+
 const getUsers = (req, res) => {
     res.json({
         message: 'List of Users'
