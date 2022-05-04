b793fc1bef6503bc8b091d006ad0782a540472b9
Stephen Bevis <stevbev95@gmail.com>
2022-05-04 10:39:01 -0600

    Setup Express Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server Is Running!')
+// Module Imports
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+// Routes
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Home route is working!'
+    })
+})
+
+app.listen(PORT, () => console.log(`Server Running: Port ${PORT}`))
