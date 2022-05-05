8c3aa11dfc0fed5969ca778fabacf2c24a3bc648
Stephen Bevis <stevbev95@gmail.com>
2022-05-04 22:18:01 -0600

    Setup Redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+
+// Reducers
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
-    },
+        auth: authReducer
+    }
 })
